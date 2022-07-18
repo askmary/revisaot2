b4f2892f2f6be3bc57c8d452be18ed24890ff553
askmary <mariana.reinaldo2003@gmail.com>
2022-07-18 15:23:14 -0300

    arrow e map com a t2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+    state = {
+       pessoas: [
+        {
+            nome: "Leonardo",
+            idade: 20,
+            cidade: "Presidente Figueiredo"
+        },
+        {
+            nome: "Henrique",
+            idade: 29,
+            cidade: "Uberlândia"
+        },
+        {
+            nome: "Luan Vitor",
+            idade: 22,
+            cidade: "Paulo Afonso"
+        },
+        {
+            nome: "Gabriel",
+            idade: 24,
+            cidade: "Rio de Janeiro"
+        },
+        {
+            nome: "Sophia",
+            idade: 22,
+            cidade: "Porto Velho"
+        }
+    ] 
+    }
+    // msg = () => {
+    //     return <h1>Olá!</h1>
+    // }
+    // add = (x, y) => {
+    //     return x + y
+    // }   
+    // double = (x) => {
+    //     return <h1>O dobro de {x} é: {2 * x}</h1>
+    // }
+    // mostrarNome = () => {
+    //     return <h2>Olá, eu sou {this.state.nome}!</h2>
+    // }
+
+    render(){
+        return(
+            <div>
+                {/* {this.msg()}
+                <h1>{this.add(7, 8)}</h1>
+                {this.double(10)}
+                {this.mostrarNome()} */}
+                {this.state.pessoas.map((item) => (
+                <div key={item.nome}>
+                    <h2>Olá, me chamo {item.nome}, tenho {item.idade} anos e moro em {item.cidade}</h2>
+                </div>
+                ))}
+            </div>
+        )
+    }
 }
-
-export default App;
